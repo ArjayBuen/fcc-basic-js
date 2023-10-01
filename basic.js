ae1beed1aaa92dce8a1c282375ffb6a9c45b94b5
ArjayBuen <arjaybuen10@gmail.com>
2023-10-01 09:46:23 +0800

    learning basic javascript

---
+++ basic.js
@@ -0,0 +1,90 @@
+var a;
+a=7; // assignment operator(=) 
+var b;
+b=a;//assigning the value of a to b
+
+var myFirstName="Rj"; //"Rj"->string literals
+var myLastName="Neub";
+
+var a=5; //  initialization
+var b=10;
+var c="I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+// arithmetic computation
+const sum = 10 + 10;
+const difference = 45 - 33;
+const product = 8 * 10;
+const quotient = 66 / 33;
+
+//increment operator ++
+let myVar = 87;
+myVar++;
+
+//decrement operator --
+let myVar1 = 11;
+myVar1--;
+
+//computing floating point numbers or floats
+const myDecimal = 5.7;
+const product2 = 2.0 * 2.5;
+const quotient2 = 4.4 / 2.0;
+
+// remainder operator %
+const remainder = 11%3;
+
+// Compound Assignment with Augmented Addition +=
+let aa = 3;
+let bb = 17;
+let cc = 12;
+
+aa += 12;
+bb += 9;
+cc += 7;
+
+// Compound Assignment With Augmented Subtraction -=
+let a2 = 11;
+let b2= 9;
+let c2 = 3;
+
+a2 -= 6;
+b2 -= 15;
+c2 -= 1;
+
+// Compound Assignment With Augmented Multiplication *=
+let a3 = 5;
+let b3 = 12;
+let c3 = 4.6;
+
+a3 *= 5;
+b3 *= 3 ;
+c3 *= 10;
+
+// Compound Assignment With Augmented Division /=
+let a4 = 48;
+let b4 = 108;
+let c4 = 33;
+
+a4 /=  12;
+b4 /= 4;
+c4 /=  11;
